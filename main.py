aa326d0529ae905012130cb5929e36dbcd2ed841
ap2322 <alicep2322@gmail.com>
2018-06-14 01:00:55 -0600

    templated web-caesar

--- main.py
+++ main.py
@@ -1,46 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from caesar import rotate_string
 import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <style>
-            form {{
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }}
-            textarea {{
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }}
-        </style>
-    </head>
-    <body>
-      <form action="/" method="POST">
-        <label for="rotate-by">Rotate by:
-            <input id="rotate-by" type="text" name="rot" value="0"/>
-        </label>
-            <textarea input id="textarea" type="textarea" name="text">{0}</textarea>
-        <input type="submit" />
-      </form>
-    </body>
-</html>
-
-"""
-
 @app.route("/")
 def index():
-    return form.format("")
+    return render_template('form.html')
 
 @app.route("/", methods=['POST'])
 def encrypt():
@@ -50,6 +17,6 @@ def encrypt():
     encrypted_text = ''
     encrypted_text += rotate_string(input_text, rot_number)
 
-    return form.format(encrypted_text)
+    return render_template('form.html', text = encrypted_text)
 
 app.run()
